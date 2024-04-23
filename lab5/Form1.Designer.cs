57ba1f4bf920599838055c5305fcdeca37d2be46
Павло Гладкий <hladkyy.p@nltu.lviv.ua>
2024-04-23 21:32:49 +0300

    Add project files.

---
+++ lab5/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            groupBox1 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            groupBox2 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            groupBox3 = new GroupBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(30, 15);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(520, 100);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Оберіть Варіант Колекцій";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(329, 42);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(93, 24);
+            radioButton2.TabIndex = 2;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Queue<>";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(85, 42);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(73, 24);
+            radioButton1.TabIndex = 1;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Queue";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Location = new Point(30, 150);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(520, 184);
+            groupBox2.TabIndex = 0;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Параметри колекцій";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(112, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(208, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Кількість елементів колекції ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(71, 82);
+            label2.Name = "label2";
+            label2.Size = new Size(249, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Номер елементу для відображень";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(339, 40);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(139, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(339, 75);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(139, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(209, 133);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 5;
+            button1.Text = "ОК";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Location = new Point(30, 366);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(520, 108);
+            groupBox3.TabIndex = 1;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Результат відображення";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(582, 503);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private GroupBox groupBox2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private GroupBox groupBox3;
+    }
+}
---
+++ lab5/Form1.cs
@@ -0,0 +1,12 @@
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
